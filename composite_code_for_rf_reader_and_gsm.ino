d334033fdee9163ff79c9488057c019b021e4c21
jnhemant <54355500+jnhemant@users.noreply.github.com>
2020-01-11 23:18:11 +0545

    Code for RF reader RC522 and GSM module SIM 800L
    Microcontroller used - Arduino Uno

--- composite_code_for_rf_reader_and_gsm.ino
+++ composite_code_for_rf_reader_and_gsm.ino
@@ -32,7 +32,7 @@ void loop() {
 
   //-------------------------------------------
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
